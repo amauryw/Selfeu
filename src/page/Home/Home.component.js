bfbb89c54e9389eb701a538b4cd5ab85865aece0
Amaury Wyart <amauryw@bam.tech>
2019-08-10 17:34:32 +0200

    init

---
+++ src/page/Home/Home.component.js
@@ -0,0 +1,13 @@
+import React from "react";
+import styled from "styled-components";
+
+export const Home = () => {
+  return (
+    <Container>
+      <WelcomeText>Couc</WelcomeText>
+    </Container>
+  );
+};
+
+const Container = styled.View``;
+const WelcomeText = styled.Text``;
